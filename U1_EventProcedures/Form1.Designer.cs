13e1bbd3d0e136ffbfbd6875753a9e02d930a245
Tan Choo Jun <cjtan@wou.edu.my>
2019-11-01 14:15:42 +0800

    Initial code check-in for Unit 1

---
+++ U1_EventProcedures/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace U105_EventProcedures
+{
+    partial class frmEvent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOne = new System.Windows.Forms.TextBox();
+            this.txtTwo = new System.Windows.Forms.TextBox();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOne
+            // 
+            this.txtOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOne.Location = new System.Drawing.Point(12, 12);
+            this.txtOne.Name = "txtOne";
+            this.txtOne.Size = new System.Drawing.Size(100, 20);
+            this.txtOne.TabIndex = 0;
+            this.txtOne.TextChanged += new System.EventHandler(this.txtOne_TextChanged);
+            // 
+            // txtTwo
+            // 
+            this.txtTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTwo.Location = new System.Drawing.Point(129, 12);
+            this.txtTwo.Name = "txtTwo";
+            this.txtTwo.Size = new System.Drawing.Size(100, 20);
+            this.txtTwo.TabIndex = 1;
+            this.txtTwo.TextChanged += new System.EventHandler(this.txtTwo_TextChanged);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(48, 71);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(146, 23);
+            this.btnGreen.TabIndex = 2;
+            this.btnGreen.Text = "Change Color To Green";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // frmEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 205);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.txtTwo);
+            this.Controls.Add(this.txtOne);
+            this.Name = "frmEvent";
+            this.Text = "Event Procedures";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOne;
+        private System.Windows.Forms.TextBox txtTwo;
+        private System.Windows.Forms.Button btnGreen;
+    }
+}
+
---
+++ U1_EventProcedures/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U105_EventProcedures
+{
+    public partial class frmEvent : Form
+    {
+        public frmEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void txtOne_TextChanged(object sender, EventArgs e)
+        {
+            txtOne.ForeColor = Color.Blue;
+        }
+
+        private void txtTwo_TextChanged(object sender, EventArgs e)
+        {
+            txtTwo.ForeColor = Color.Red;
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            txtOne.ForeColor = Color.Green;
+            //txtOne.Font.Bold = false;
+            txtTwo.ForeColor = Color.Green;
+        }
+    }
+}
