02673d2cf65c02aa252df2a5b91c6f7c54b642dc
Tan Choo Jun <cjtan@wou.edu.my>
2019-12-26 00:59:37 -0800

    Code check-in for Unit 2

---
+++ U2_Method/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2_Method
+{
+    class Program
+    {
+        static int SumMe(int a, int b)
+        {
+            int add = a + b;
+            return add;
+        }
+        static int MultiplyMe(int a, int b = 2)
+        {
+            int multiply = a * b;
+            return multiply;
+        }
+        static void Main(string[] args)
+        {
+            int number1 = 3;
+            int number2 = 6;
+            int total1 = SumMe(number1, number2);
+            Console.WriteLine("total1: {0}", total1);
+
+            // Named Argument
+            int total2 = SumMe(b: 5, a: 10);
+            Console.WriteLine("total2: {0}", total2);
+
+            // Optional Argument
+            int total3 = MultiplyMe(7); // a = 7, b = 2
+            Console.WriteLine("total3: {0}", total3);
+            int total4 = MultiplyMe(7, 3); // a = 7, b = 3
+            Console.WriteLine("total4: {0}", total4);
+        }
+    }
+}
---
+++ U2_MethodPassByRef/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2_MethodPassByRef
+{
+    class Program
+    {
+        static void PassByRef(ref int i)
+        {
+            i = i + 1;
+        }
+        static void OutMethod(out int i)
+        {
+            i = 1;
+        }
+        static void Main(string[] args)
+        {
+            int j = 0;
+            PassByRef(ref j);
+            Console.WriteLine(j); //j = 1
+
+            int k;
+
+            OutMethod(out k);
+            Console.WriteLine(k); // k = 1
+        }
+    }
+}
